c0a0446b0cc9c51adfdc5891a2a03d1a5905bf9f
Sam Popowich <spopowich@gmail.com>
2014-04-10 15:18:22 -0600

    Added first two pending tests.

---
+++ MODS/spec/spec.rb
@@ -0,0 +1,13 @@
+require "mods"
+
+describe "read in and print a MODS record" do
+  context "when the file is open" do
+    it "should read the data and create a MODS record out of it" 
+  end
+
+  context "when provided with a MODS record" do
+    it "should print the complete MODS record to the screen" 
+  end
+end
+
+
