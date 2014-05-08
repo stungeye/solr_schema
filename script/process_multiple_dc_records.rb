3704729d3ab300e31d790247829358b26db776c0
Sam Popowich <spopowich@gmail.com>
2014-05-08 12:27:35 -0600

    Added basic usage scripts.

---
+++ script/process_multiple_dc_records.rb
@@ -0,0 +1,23 @@
+require "nokogiri"
+require_relative "../lib/dublin_core.rb"
+
+class String
+  def blank?
+    respond_to?(:empty?) ? empty? : !self
+  end
+end
+
+record_set = Nokogiri::XML(File.open(ARGV[0]).read.gsub("\n", "")).remove_namespaces!.xpath("//record")
+solr_record_set = []
+
+record_set.each do |record|
+  solr_record_set << DublinCoreRecord.new(record.to_xml)
+end
+
+File.open("solr.xml", "w"){|f|
+  f.puts %Q[<?xml version="1.0" encoding="UTF-8"?><add>]
+  solr_record_set.each do |record|
+    f.puts record.to_solr
+  end
+  f.puts %Q[</add>]
+}
